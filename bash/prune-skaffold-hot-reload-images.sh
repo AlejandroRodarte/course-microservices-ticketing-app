752b181b468ee2a08a1e88bcde87a577eaf18f4b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-03-21 10:10:07 -0700

    integrate skaffold (took a lot of work)

---
+++ bash/prune-skaffold-hot-reload-images.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bash ~/Documents/personal/scripts/bash/microk8s/remove-all-images-from-local-registry/script.sh
+bash ~/Documents/personal/scripts/bash/docker/remove-all-images-from-microk8s-registry/script.sh
