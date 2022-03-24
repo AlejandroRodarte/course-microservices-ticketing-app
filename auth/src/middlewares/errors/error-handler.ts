9e3921b6583f2bca5cf621e9827b027fc3962cca
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-03-24 09:48:22 -0700

    139. Communicating More Info to the Error Handler

--- auth/src/middlewares/errors/error-handler.ts
+++ auth/src/middlewares/errors/error-handler.ts
@@ -2,7 +2,7 @@ import { ErrorRequestHandler } from 'express';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.log('err: ', err);
-  return res.status(200).send({ message: 'Something went wrong!' });
+  if (err instanceof Error) return res.status(200).send({ message: err.message });
 };
 
 export default errorHandler;
