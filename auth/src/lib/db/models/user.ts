53b3449834c727e99e7ce9ec7c671c28a8646864
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-03-29 20:16:06 -0700

    164. Mongoose Pre-Save Hooks

--- auth/src/lib/db/models/user.ts
+++ auth/src/lib/db/models/user.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { DbModelTypes } from '../../types/db/models';
+import bcrypt from '../../bcrypt';
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,9 +17,19 @@ userSchema.statics.build = function (attributes: DbModelTypes.UserAttributes) {
   return new User(attributes);
 };
 
-const User = mongoose.model<
-  DbModelTypes.UserDocument,
-  DbModelTypes.UserModel
->('User', userSchema);
+userSchema.pre<DbModelTypes.UserDocument>('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    const [hashedPassword, bcryptError] = await bcrypt.hash(user.password);
+    if (typeof hashedPassword === 'undefined' && bcryptError) next(bcryptError);
+    user.password = hashedPassword!;
+    next();
+  }
+});
+
+const User = mongoose.model<DbModelTypes.UserDocument, DbModelTypes.UserModel>(
+  'User',
+  userSchema
+);
 
 export default User;
