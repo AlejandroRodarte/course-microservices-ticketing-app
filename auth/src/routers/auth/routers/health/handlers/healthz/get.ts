b041c46071300848613de3f99f46517cfa778ca0
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-02 20:40:37 -0700

    attempt 4 to make ci work

--- auth/src/routers/auth/routers/health/handlers/healthz/get.ts
+++ auth/src/routers/auth/routers/health/handlers/healthz/get.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 
 const get: RequestHandler = (req, res) => {
-  return res.status(200).send({ message: 'OK' });
+  return res.status(200).send({ message: 'OK!' });
 };
 
 export default get;
