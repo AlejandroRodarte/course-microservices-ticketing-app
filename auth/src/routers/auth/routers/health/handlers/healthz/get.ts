670276a233f06d4ac6653b2cc35d5d263453aa56
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-02 21:10:34 -0700

    update auth/health/healthz to send ApplicationResponse

--- auth/src/routers/auth/routers/health/handlers/healthz/get.ts
+++ auth/src/routers/auth/routers/health/handlers/healthz/get.ts
@@ -1,7 +1,18 @@
 import { RequestHandler } from 'express';
+import ApplicationResponse from '../../../../../../lib/objects/application-response';
 
 const get: RequestHandler = (req, res) => {
-  return res.status(200).send({ message: 'OK!' });
+  return res
+    .status(200)
+    .send(
+      new ApplicationResponse<undefined, undefined>(
+        200,
+        'APPLICATION_HEALTHY',
+        'The application is healthy.',
+        undefined,
+        undefined
+      )
+    );
 };
 
 export default get;
