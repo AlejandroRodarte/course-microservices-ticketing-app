03ab8e8d67c734d70a0ab91675590d5542913698
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-04 18:30:56 -0600

    215. Note on File Change Detection

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (dev) config.watchOptions.poll = 300;
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
