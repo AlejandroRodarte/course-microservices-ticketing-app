11b328bde095f8b9626d42896dddedf675dcf56d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-20 15:36:51 -0600

    239. Moving GetInitialProps

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import DefaultLayout from '../components/layouts/default-layout';
+import { NextPageContext } from 'next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,5 +11,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     </DefaultLayout>
   );
 }
+
+MyApp.getInitialProps = async (ctx: NextPageContext) => {
+  console.log('getInitialProps called in _app.tsx');
+  return { props: {} };
+};
 
 export default MyApp;
