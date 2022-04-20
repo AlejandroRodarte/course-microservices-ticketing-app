ee646b7618fb58ab8e424b0b573b0a60722c0143
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-20 15:55:01 -0600

    240. Issues with Custom App GetInitialProps

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
+import https from 'https';
 import type { AppProps } from 'next/app';
 import DefaultLayout from '../components/layouts/default-layout';
-import { NextPageContext } from 'next';
+import { AppContextType } from 'next/dist/shared/lib/utils';
+import doServerSideRequest from '../lib/requests/do-server-side-request';
+import CurrentUserData from '../lib/objects/data/auth/current-user-data';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,8 +15,31 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-MyApp.getInitialProps = async (ctx: NextPageContext) => {
+MyApp.getInitialProps = async ({ ctx }: AppContextType) => {
   console.log('getInitialProps called in _app.tsx');
+  const [response, error] = await doServerSideRequest<
+    undefined,
+    CurrentUserData
+  >({
+    endpoint: 'auth/users/current-user',
+    microservice: 'auth',
+    method: 'get',
+    config: {
+      withCredentials: true,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+      headers: {
+        Cookie:
+          ctx && ctx.req && ctx.req.headers.cookie
+            ? ctx.req.headers.cookie
+            : '',
+      },
+    },
+  });
+  console.log(response);
+  // if (response && response.status === 200 && response.data)
+  //   return { props: { user: response.data.user } };
   return { props: {} };
 };
 
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -12,29 +12,4 @@ const HomePage: React.FC<HomePageProps> = (props) => {
   return <h1> You are {props.user ? '' : 'NOT'} signed in.</h1>;
 };
 
-export const getServerSideProps: GetServerSideProps<HomePageProps> = async (
-  ctx
-) => {
-  const [response, error] = await doServerSideRequest<
-    undefined,
-    CurrentUserData
-  >({
-    endpoint: 'auth/users/current-user',
-    microservice: 'auth',
-    method: 'get',
-    config: {
-      withCredentials: true,
-      httpsAgent: new https.Agent({
-        rejectUnauthorized: false,
-      }),
-      headers: {
-        Cookie: ctx.req.headers.cookie || '',
-      },
-    },
-  });
-  if (response && response.status === 200 && response.data)
-    return { props: { user: response.data.user } };
-  return { props: { user: null } };
-};
-
 export default HomePage;
