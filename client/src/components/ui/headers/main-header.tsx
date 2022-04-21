6d784991f1f79f600e678143e2d729b68ce990c2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-20 19:17:05 -0600

    244. Conditionally Showing Links

--- client/src/components/ui/headers/main-header.tsx
+++ client/src/components/ui/headers/main-header.tsx
@@ -1,11 +1,30 @@
 import Link from 'next/link';
 import BaseUserDto from '../../../lib/objects/dto/auth/base-user-dto';
+import { UIComponentTypes } from '../../../lib/types/components/ui';
 
 interface MainHeaderProps {
   user: BaseUserDto | null;
 }
 
 const MainHeader: React.FC<MainHeaderProps> = (props) => {
+  const links: UIComponentTypes.HeaderLink[] = [
+    {
+      href: '/auth/sign-up',
+      label: 'Sign Up',
+      show: !!!props.user,
+    },
+    {
+      href: '/auth/sign-in',
+      label: 'Sign In',
+      show: !!!props.user,
+    },
+    {
+      href: '/auth/sign-out',
+      label: 'Sign Out',
+      show: !!props.user,
+    },
+  ];
+
   return (
     <nav className="p-3 navbar navbar-light bg-light">
       <Link href="/">
@@ -13,7 +32,16 @@ const MainHeader: React.FC<MainHeaderProps> = (props) => {
       </Link>
       <div className="d-flex justify-content-end">
         <ul className="nav d-flex align-items-center">
-          {props.user ? 'Sign Out' : 'Sign In/Sign Up'}
+          {links.map(
+            (link) =>
+              link.show && (
+                <li className="nav-item" key={link.label}>
+                  <Link href={link.href}>
+                    <a className="nav-link">{link.label}</a>
+                  </Link>
+                </li>
+              )
+          )}
         </ul>
       </div>
     </nav>
---
+++ client/src/lib/types/components/ui.ts
@@ -0,0 +1,7 @@
+export namespace UIComponentTypes {
+  export interface HeaderLink {
+    label: string;
+    show: boolean;
+    href: string;
+  }
+}
