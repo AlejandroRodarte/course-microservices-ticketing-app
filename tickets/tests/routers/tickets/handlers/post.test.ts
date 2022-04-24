4edb93b0503df83d401f25a65e62164f72ebb587
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-23 18:35:46 -0600

    264. Creating the Router

--- tickets/tests/routers/tickets/handlers/post.test.ts
+++ tickets/tests/routers/tickets/handlers/post.test.ts
@@ -1,3 +1,4 @@
+import { ApplicationResponseTypes, objects } from '@msnr-ticketing-app/common';
 import request from 'supertest';
 import app from '../../../../src/app';
 
@@ -5,7 +6,16 @@ const route = '/tickets';
 
 describe('Tests for the POST /tickets endpoint.', () => {
   describe('Success cases', () => {
-    it('Should have a route handler listening on /tickets for POST requests.', async () => {});
+    it('Should have a route handler listening on /tickets for POST requests.', async () => {
+      const response = await request(app).post(route).send({}).expect(200);
+      const applicationResponse =
+        response.body as ApplicationResponseTypes.Body<
+          unknown,
+          InstanceType<typeof objects.errors.UniversalError>
+        >;
+      expect(applicationResponse.status).not.toBe(404);
+      expect(applicationResponse.code).not.toBe('ROUTE_NOT_FOUND');
+    });
     it('Should persist a new ticket if all parameters are valid', async () => {});
   });
 
