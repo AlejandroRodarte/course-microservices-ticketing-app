ac12681ee68e2503fc59004b88ca0aa1a03d70cb
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-26 16:50:24 -0600

    288. Building a NATS Test Project

---
+++ playground/nats-test/src/publisher.ts
@@ -0,0 +1,14 @@
+import nats from 'node-nats-streaming';
+
+// nats.connect(clusterID, clientID, { url: natsStreamingServerURL })
+// clusterID was defined in our nats-deployment pod spec with the -cid flag
+// clientID is any unique identifier you desire to provide to your NATS client
+// natsStreamingServerURL is the URL where your NATS Streaming Server is located
+// in the case of K8S, it would be something like http://nats-service:4222
+const client = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222',
+});
+
+client.on('connect', () => {
+  console.log('Published connected to NATS.');
+});
