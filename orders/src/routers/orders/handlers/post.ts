2cf4e3f65e20eacd9bb0691a605984f35095c6e5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-19 14:07:02 -0600

    354. Finding Reserved Tickets

--- orders/src/routers/orders/handlers/post.ts
+++ orders/src/routers/orders/handlers/post.ts
@@ -1,4 +1,4 @@
-import { db, objects } from '@msnr-ticketing-app/common';
+import { constants, db, objects } from '@msnr-ticketing-app/common';
 import { Response } from 'express';
 import Order from '../../../lib/db/models/order';
 import Ticket from '../../../lib/db/models/ticket';
@@ -28,6 +28,31 @@ const post = async (
     );
 
   // 2. make sure that the ticket is not already reserved
+  // 2a. find order that has ticket we just found as that is not reserved (non-cancelled status)
+  const [existingOrder, findOneExistingOrderError] = await db.helpers.findOne<
+    DbModelTypes.OrderDocument,
+    DbModelTypes.OrderModel
+  >({
+    Model: Order,
+    filters: {
+      ticket,
+      status: {
+        $in: constants.resources.order.statuses.filter(
+          (status) => status !== 'cancelled'
+        ),
+      },
+    },
+    errorMessage: `Error finding order that matches ticket ID ${ticket._id} and that is not cancelled.`,
+  });
+
+  if (findOneExistingOrderError) throw findOneExistingOrderError;
+  // 2b. if a non-cancelled order is found for the ticket, it means that the ticket has been reserver
+  if (existingOrder)
+    throw new objects.errors.BadEntityError(
+      'order',
+      `There is already an order reserving ticket with ID ${ticket._id}`
+    );
+
   // 3. calculate an expiration date for this order
   // 4. build the order and save it to the database
   // 5. publish event informing an order has been created
