cfaa3e39eec2c4b2c800d6e4c0b38c0e77bf89de
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-19 16:05:18 -0600

    Detener 360. Asserting Tickets Exist

--- orders/tests/routers/orders/handlers/post.test.ts
+++ orders/tests/routers/orders/handlers/post.test.ts
@@ -1,4 +1,5 @@
-import { ApplicationResponseTypes } from '@msnr-ticketing-app/common';
+import { ApplicationResponseTypes, objects } from '@msnr-ticketing-app/common';
+import mongoose from 'mongoose';
 import request from 'supertest';
 import app from '../../../../src/app';
 import cookies from '../../../lib/cookies';
@@ -21,5 +22,117 @@ describe('Test for the POST /orders endpoint.', () => {
       expect(applicationResponse.status).not.toBe(404);
       expect(applicationResponse.code).not.toBe('ROUTE_NOT_FOUND');
     });
+
+    it('Should return a 201/ORDER_CREATED status if the order is succesfully created', async () => {});
+  });
+
+  describe('Route handler errors', () => {
+    it('Should return a 401/UNAUTHORIZED_ERROR status if the request has no credentials.', async () => {
+      const body = {
+        data: {
+          newOrder: {
+            ticketId: new mongoose.Types.ObjectId().toHexString(),
+          },
+        },
+      };
+
+      const response = await request(app)
+        .post(routes.newOrder)
+        .send(body)
+        .expect(200);
+      const applicationResponse =
+        response.body as ApplicationResponseTypes.Body<
+          undefined,
+          InstanceType<typeof objects.errors.UniversalError>
+        >;
+
+      expect(applicationResponse.status).toBe(401);
+      expect(applicationResponse.code).toBe('UNAUTHORIZED_ERROR');
+    });
+
+    it('Should return a 422/VALIDATION_ERROR status if no ticketId is provided in the request body.', async () => {
+      const [, cookie] = cookies.helpers.createUserAndCookie();
+      const body = {
+        data: {
+          newOrder: {},
+        },
+      };
+
+      const response = await request(app)
+        .post(routes.newOrder)
+        .send(body)
+        .set('Cookie', cookie)
+        .expect(200);
+      const applicationResponse =
+        response.body as ApplicationResponseTypes.Body<
+          undefined,
+          InstanceType<typeof objects.errors.UniversalError>
+        >;
+
+      expect(applicationResponse.status).toBe(422);
+      expect(applicationResponse.code).toBe('VALIDATION_ERROR');
+    });
+
+    it('Should return a 422/VALIDATION_ERROR status the ticketId does not match MongoDB standard.', async () => {
+      const [, cookie] = cookies.helpers.createUserAndCookie();
+      const body = {
+        data: {
+          newOrder: {
+            ticketId: '123abc',
+          },
+        },
+      };
+
+      const response = await request(app)
+        .post(routes.newOrder)
+        .send(body)
+        .set('Cookie', cookie)
+        .expect(200);
+      const applicationResponse =
+        response.body as ApplicationResponseTypes.Body<
+          undefined,
+          InstanceType<typeof objects.errors.UniversalError>
+        >;
+
+      expect(applicationResponse.status).toBe(422);
+      expect(applicationResponse.code).toBe('VALIDATION_ERROR');
+
+      const ticketIdError = applicationResponse.error.errors.find(
+        (error) => error.field === 'data.newOrder.ticketId'
+      );
+      expect(ticketIdError!.message).toBe(
+        'A ticket ID should be supplied and match a MongoDB ObjectID structure.'
+      );
+    });
+
+    it('Should return a 404/ENTITY_NOT_FOUND_ERROR status if the ticket is not found in the database.', async () => {
+      const [, cookie] = cookies.helpers.createUserAndCookie();
+      const body = {
+        data: {
+          newOrder: {
+            ticketId: new mongoose.Types.ObjectId().toHexString(),
+          },
+        },
+      };
+
+      const response = await request(app)
+        .post(routes.newOrder)
+        .send(body)
+        .set('Cookie', cookie)
+        .expect(200);
+      const applicationResponse =
+        response.body as ApplicationResponseTypes.Body<
+          undefined,
+          InstanceType<typeof objects.errors.UniversalError>
+        >;
+
+      expect(applicationResponse.status).toBe(404);
+      expect(applicationResponse.code).toBe('ENTITY_NOT_FOUND_ERROR');
+
+      const [error] = applicationResponse.error.errors;
+      expect(error.field).toBe('ticket');
+    });
+
+    it('Should return a 400/BAD_ENTITY_ERROR status if the ticket is already reserved.', async () => {});
   });
 });
