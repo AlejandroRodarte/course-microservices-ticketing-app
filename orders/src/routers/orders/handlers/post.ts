5c5751c48b8bdbfdd5352a35c59014d791e5ec21
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-19 13:48:25 -0600

    Reproducir 353. Order Creation Logic

--- orders/src/routers/orders/handlers/post.ts
+++ orders/src/routers/orders/handlers/post.ts
@@ -1,11 +1,37 @@
-import { objects } from '@msnr-ticketing-app/common';
+import { db, objects } from '@msnr-ticketing-app/common';
 import { Response } from 'express';
+import Order from '../../../lib/db/models/order';
+import Ticket from '../../../lib/db/models/ticket';
+import { DbModelTypes } from '../../../lib/types/db/models';
 import { OrdersRequestHandlers } from '../../../lib/types/request-handlers/orders';
 
 const post = async (
   req: OrdersRequestHandlers.PostOrdersExtendedRequest,
   res: Response
 ) => {
+  // 1. find ticket user is trying to order in database
+  const { ticketId } = req.body.data.newOrder;
+  const [ticket, findOneTicketError] = await db.helpers.findById<
+    DbModelTypes.TicketDocument,
+    DbModelTypes.TicketModel
+  >({
+    Model: Ticket,
+    id: ticketId,
+    errorMessage: `There was an error finding ticket with ID ${ticketId}.`,
+  });
+
+  if (findOneTicketError) throw findOneTicketError;
+  if (!ticket)
+    throw new objects.errors.EntityNotFoundError(
+      'ticket',
+      `Ticket with ID ${ticketId} was not found in the database.`
+    );
+
+  // 2. make sure that the ticket is not already reserved
+  // 3. calculate an expiration date for this order
+  // 4. build the order and save it to the database
+  // 5. publish event informing an order has been created
+
   return res
     .status(200)
     .send(
