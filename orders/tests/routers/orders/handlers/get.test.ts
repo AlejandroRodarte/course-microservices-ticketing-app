49e9a566ed37b20b745dd358a662f6f1751bba0e
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-19 20:05:06 -0600

    364. A Slightly Complicated Test

--- orders/tests/routers/orders/handlers/get.test.ts
+++ orders/tests/routers/orders/handlers/get.test.ts
@@ -2,6 +2,11 @@ import { ApplicationResponseTypes } from '@msnr-ticketing-app/common';
 import request from 'supertest';
 import app from '../../../../src/app';
 import cookies from '../../../lib/cookies';
+import Order from '../../../../src/lib/db/models/order';
+import Ticket from '../../../../src/lib/db/models/ticket';
+import { DbModelTypes } from '../../../../src/lib/types/db/models';
+import GetOrdersData from '../../../../src/lib/objects/data/orders/get-orders-data';
+import BaseTicketDto from '../../../../src/lib/objects/dto/tickets/base-ticket-dto';
 
 const routes = {
   newOrder: '/orders',
@@ -17,6 +22,78 @@ describe('Test for the GET /orders endpoint.', () => {
 
       expect(applicationResponse.status).not.toBe(404);
       expect(applicationResponse.code).not.toBe('ROUTE_NOT_FOUND');
+    });
+
+    it('Should return a 200/USER_ORDERS_FETCHED', async () => {
+      const [, firstUserCookie] = cookies.helpers.createUserAndCookie();
+      const [secondUser, secondUserCookie] =
+        cookies.helpers.createUserAndCookie();
+
+      // 1. create three tickets
+      const ticketAttributes: DbModelTypes.TicketAttributes[] = [
+        {
+          title: 'Concert',
+          price: 20,
+        },
+        {
+          title: 'Art Gallery',
+          price: 50,
+        },
+        {
+          title: 'Monster Jam',
+          price: 35,
+        },
+      ];
+
+      const tickets: DbModelTypes.TicketDocument[] = [];
+
+      for (const ticketAttribute of ticketAttributes) {
+        const ticket = Ticket.build(ticketAttribute);
+        const savedTicket = await ticket.save();
+        tickets.push(savedTicket);
+      }
+
+      // 2. create one order as user 1
+      await request(app)
+        .post(routes.newOrder)
+        .send({ data: { newOrder: { ticketId: tickets[0]._id } } })
+        .set('Cookie', firstUserCookie)
+        .expect(200);
+
+      // 3. create two orders as user 2
+      await request(app)
+        .post(routes.newOrder)
+        .send({ data: { newOrder: { ticketId: tickets[1]._id } } })
+        .set('Cookie', secondUserCookie)
+        .expect(200);
+      await request(app)
+        .post(routes.newOrder)
+        .send({ data: { newOrder: { ticketId: tickets[2]._id } } })
+        .set('Cookie', secondUserCookie)
+        .expect(200);
+
+      // 4. query orders for user 2
+      const response = await request(app)
+        .get(routes.getOrders)
+        .set('Cookie', secondUserCookie)
+        .expect(200);
+      const applicationResponse =
+        response.body as ApplicationResponseTypes.Body<
+          GetOrdersData,
+          undefined
+        >;
+
+      expect(applicationResponse.status).toBe(200);
+      expect(applicationResponse.code).toBe('USER_ORDERS_FETCHED');
+
+      const userIds = applicationResponse.data.orders.map(
+        (order) => order.userId
+      );
+      for (const userId of userIds) expect(userId).toBe(secondUser.id);
+
+      const [firstOrder, secondOrder] = applicationResponse.data.orders;
+      expect(firstOrder.ticket.id).toBe(tickets[1].id);
+      expect(secondOrder.ticket.id).toBe(tickets[2].id);
     });
   });
 });
