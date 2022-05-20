539bb25223a95331bf1f85f7ac57befe3b1b21be
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-20 09:44:53 -0600

    379. Simple onMessage Implementation

--- orders/src/lib/objects/nats/listeners/ticket-created-listener.ts
+++ orders/src/lib/objects/nats/listeners/ticket-created-listener.ts
@@ -1,6 +1,8 @@
-import { NatsTypes, objects } from '@msnr-ticketing-app/common';
+import { db, NatsTypes, objects } from '@msnr-ticketing-app/common';
 import { Message } from 'node-nats-streaming';
 import { QUEUE_GROUP_NAME } from '../../../constants/nats';
+import Ticket from '../../../db/models/ticket';
+import { DbModelTypes } from '../../../types/db/models';
 
 class TicketCreatedListener extends objects.nats
   .Listener<NatsTypes.TicketCreatedEvent> {
@@ -10,7 +12,26 @@ class TicketCreatedListener extends objects.nats
   readonly queueGroupName = QUEUE_GROUP_NAME;
 
   onMessage(msg: Message, data: NatsTypes.TicketCreatedEventData): void {
-    throw new Error('Method not implemented.');
+    this.exec(msg, data).then((err) => {
+      if (!err) msg.ack();
+    });
+  }
+
+  private async exec(
+    msg: Message,
+    data: NatsTypes.TicketCreatedEventData
+  ): Promise<
+    InstanceType<typeof objects.errors.DatabaseOperationError> | undefined
+  > {
+    const { title, price } = data;
+    const ticket = Ticket.build({ title, price });
+    const [, saveTicketError] =
+      await db.helpers.save<DbModelTypes.TicketDocument>({
+        document: ticket,
+        errorMessage: 'There was an error saving the ticket into the database.',
+      });
+    if (saveTicketError) return saveTicketError;
+    return undefined;
   }
 }
 
