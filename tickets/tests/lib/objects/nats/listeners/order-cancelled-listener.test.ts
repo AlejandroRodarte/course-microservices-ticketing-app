7d890d702540cbf35935c6725bbe80ce41370fa5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-22 10:25:46 -0600

    419. A Lightning-Quick Test

---
+++ tickets/tests/lib/objects/nats/listeners/order-cancelled-listener.test.ts
@@ -0,0 +1,98 @@
+import { NatsTypes } from '@msnr-ticketing-app/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import Ticket from '../../../../../src/lib/db/models/ticket';
+import OrderCancelledListener from '../../../../../src/lib/objects/nats/listeners/order-cancelled-listener';
+import stanSingleton from '../../../../../src/lib/objects/nats/stan-singleton';
+
+describe('Tests for the OrderCancelledListener object.', () => {
+  const setup = async () => {
+    const [stan] = stanSingleton.stan;
+    const listener = new OrderCancelledListener(stan!);
+
+    const savedTicket = Ticket.build({
+      title: 'Concert',
+      price: 20,
+      userId: new mongoose.Types.ObjectId().toHexString(),
+    });
+    await savedTicket.save();
+
+    const orderCancelledEventArray: NatsTypes.OrderCancelledEventData[] = [
+      {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        ticket: {
+          id: savedTicket.id,
+        },
+      },
+      {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        ticket: {
+          id: new mongoose.Types.ObjectId().toHexString(),
+        },
+      },
+    ];
+
+    // @ts-ignore
+    const msg: Message = {
+      ack: jest.fn(),
+    };
+
+    return { listener, savedTicket, orderCancelledEventArray, msg };
+  };
+
+  describe('Success cases', () => {
+    it('Should un-reserve ticket by removing the related orderId.', async () => {
+      const { listener, savedTicket, orderCancelledEventArray, msg } =
+        await setup();
+      const [data] = orderCancelledEventArray;
+
+      await listener.onMessage(msg, data);
+
+      const ticket = await Ticket.findById(savedTicket.id);
+      expect(ticket?.orderId).toBeUndefined();
+    });
+
+    it('Should emit a ticket:updated event once the ticket has been un-reserved.', async () => {
+      const { listener, savedTicket, orderCancelledEventArray, msg } =
+        await setup();
+      const [data] = orderCancelledEventArray;
+
+      await listener.onMessage(msg, data);
+
+      const ticket = await Ticket.findById(savedTicket.id);
+
+      const [stan] = stanSingleton.stan;
+      expect(stan!.publish).toHaveBeenCalledWith(
+        'ticket:updated',
+        JSON.stringify({
+          id: ticket!.id,
+          title: ticket!.title,
+          price: ticket!.price,
+          userId: ticket!.userId,
+          version: ticket!.version,
+        }),
+        expect.any(Function)
+      );
+    });
+
+    it('Should acknowledge the message after the ticket is reserved.', async () => {
+      const { listener, orderCancelledEventArray, msg } = await setup();
+      const [data] = orderCancelledEventArray;
+
+      await listener.onMessage(msg, data);
+      expect(msg.ack).toHaveBeenCalled();
+    });
+  });
+
+  describe('Failure cases', () => {
+    it('Should not acknowledge the message if no ticket is found in the database.', async () => {
+      const { listener, orderCancelledEventArray, msg } = await setup();
+      const [, data] = orderCancelledEventArray;
+
+      await listener.onMessage(msg, data);
+      expect(msg.ack).not.toHaveBeenCalled();
+    });
+  });
+});
