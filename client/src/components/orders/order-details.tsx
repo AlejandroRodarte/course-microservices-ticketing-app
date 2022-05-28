97e3529fdfaec89d75616477d985b26a8eae7e50
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-28 12:39:56 -0600

    483. Displaying the Expiration

--- client/src/components/orders/order-details.tsx
+++ client/src/components/orders/order-details.tsx
@@ -8,14 +8,14 @@ interface OrderDetailsProps {
 const OrderDetails: React.FC<OrderDetailsProps> = (props) => {
   const { order } = props;
 
-  const [timeLeft, setTimeLeft] = useState('');
+  const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
     const findTimeLeft = () => {
       setTimeLeft(() =>
         Math.round(
           (new Date(order.expiresAt).getTime() - new Date().getTime()) / 1000
-        ).toString()
+        )
       );
     };
 
@@ -28,6 +28,7 @@ const OrderDetails: React.FC<OrderDetailsProps> = (props) => {
     };
   }, [setTimeLeft, order.expiresAt]);
 
+  if (timeLeft < 0) return <div>Order Expired</div>;
   return <div>Time left to pay: {timeLeft} seconds.</div>;
 };
 
