64f33eca0f990846e95360ffc4772783b49c7337
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-30 09:48:50 -0600

    secure cookie enabled on production node_env

--- payments/src/lib/options/cookie-session/options.ts
+++ payments/src/lib/options/cookie-session/options.ts
@@ -1,8 +1,10 @@
+const validEnvs = ['production', 'development-gcloud'];
+
 const secure =
-  process.env.NODE_ENV === 'development-gcloud' &&
+  validEnvs.includes(process.env.NODE_ENV!) &&
   (!process.env.FORCE_INSECURE_COOKIE ||
     process.env.FORCE_INSECURE_COOKIE === 'false');
-const sameSite = process.env.NODE_ENV === 'development-gcloud' ? 'none' : 'lax';
+const sameSite = validEnvs.includes(process.env.NODE_ENV!) ? 'none' : 'lax';
 
 const cookieSessionOptions: CookieSessionInterfaces.CookieSessionOptions = {
   signed: false,
