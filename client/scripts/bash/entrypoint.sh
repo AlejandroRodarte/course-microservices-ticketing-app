a1dab5e8f8e6e3857da1cb1cfae8e7ce9c0e1ec5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-31 20:38:11 -0600

    try to inject NEXT_PUBLIC env variables with entrypoint script

---
+++ client/scripts/bash/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -Ex
+
+function apply_path {
+  echo "Check that we have NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY variable."
+  test -n "$NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY"
+  find /node/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_VAR#$NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY#g"
+
+  echo "Check that we have NEXT_PUBLIC_ENV variable."
+  test -n "$NEXT_PUBLIC_ENV"
+  find /node/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_ENV_VAR#$NEXT_PUBLIC_ENV#g"
+
+  echo "Check that we have NEXT_PUBLIC_API_URL variable."
+  test -n "$NEXT_PUBLIC_API_URL"
+  find /node/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_API_URL_VAR#$NEXT_PUBLIC_API_URL#g"
+}
+
+apply_path
+
